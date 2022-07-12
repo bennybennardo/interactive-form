e65002fab037ee4aaa6e878d06b64b2f1a84b069
bennybennardo <professorbennardo@gmail.com>
2022-07-11 21:16:31 -0500

    Updated 'Other' visibility in Job Role

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('test')
+//Focuses on the 'Name' input upon page load
+
+let userName = document.getElementById('name');
+userName.focus();
+
+//job role
+
+let jobRole = document.getElementById('title');
+let otherIndex = (jobRole.length)-1
+let otherInput = document.getElementById('other-job-role')
+
+otherInput.style.display = 'none';
+
+jobRole.addEventListener('click', () => {
+    let userJob = jobRole.selectedIndex
+
+    if ( userJob === otherIndex ) {
+        otherInput.style.display = 'block';
+    } else {
+        otherInput.style.display = 'none';
+    }
+});
+
+
+
+
+
